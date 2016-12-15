9673d431023ee6e7b2586e4e89931a26aa1e82dc
dikys <lrdikysa@mail.ru>
2016-12-15 20:18:09 +0500

    Улучшил интерфейс, реализовал выбор команды из списка, осталось его перетащить

---
+++ TeachMe/TeachMe.Appl/Command.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TeachMe.Appl
+{
+    public class Command
+    {
+        // ТУТ ПРОБЛЕМКА ПОТОМ БУДЕТ куда индекс девать? Надо присваивать где-то и изменять нужно
+        // А если его нужно менять, то сделать проверку целостности
+        public Command(Command command)
+        {
+            this.Name = command.Name;
+            this.Description = command.Description;
+            this.Image = command.Image;
+            this.Method = command.Method;
+        }
+
+        public Command(int index, string name, string description, BitmapImage image, Action method)
+        {
+            this.Index = index;
+            this.Name = name;
+            this.Description = description;
+            this.Image = image;
+            this.Method = method;
+        }
+        
+        public int Index { get; }
+        public string Description { get; }
+        public string Name { get; }
+        public BitmapImage Image { get; }
+        public Action Method { get; }
+    }
+}
--- TeachMe/TeachMe.Appl/MainWindow.xaml.cs
+++ TeachMe/TeachMe.Appl/MainWindow.xaml.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 using TeachMe.Domain;
-using TeachMe.Infrastructure;
+using Transform = TeachMe.Infrastructure.Transform;
 
 namespace TeachMe.Appl
 {
@@ -12,12 +19,19 @@ namespace TeachMe.Appl
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Command> RobotCommands;
+        public ObservableCollection<Command> CurrentCommands; 
+
+        private Command _dragged;
+
+        private GameModel _gameModel;
+
         public MainWindow()
         {
             InitializeComponent();
             
             this.MouseLeftButtonDown += (sender, args) => this.DragMove();
-            this.FoldingButton.Click +=
+            /*this.FoldingButton.Click +=
                 (sender, args) =>
                     this.WindowState =
                         this.WindowState == WindowState.Minimized ? WindowState.Normal : WindowState.Minimized;
@@ -25,7 +39,85 @@ namespace TeachMe.Appl
                 (sender, args) =>
                     this.WindowState =
                         this.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
-            this.ClosingButton.Click += (sender, args) => this.Close();
+            this.ClosingButton.Click += (sender, args) => this.Close();*/
+            
+            this.AllCommands.PreviewMouseLeftButtonDown += (sender, e) =>
+            {
+                if (this._dragged != null)
+                    return;
+
+                var element = this.AllCommands.InputHitTest(e.GetPosition(this.AllCommands)) as UIElement;
+
+                while (element != null)
+                {
+                    if (element is ListBoxItem)
+                    {
+                        var item = (ListBoxItem) element;
+
+                        if (!(item.Content is Command))
+                            return;
+
+                        var command = (Command)item.Content;
+
+                        this._dragged = command;
+
+                        /*var grid = (Grid) item.Content;
+                        
+                        var textBlockWithIndex = grid.Children[1] as TextBlock;
+
+                        if (textBlockWithIndex == null)
+                            throw new NullReferenceException("textBlockWithIndex");
+
+                        int index;
+                        if (!Int32.TryParse(textBlockWithIndex.Text, out index))
+                            throw new FormatException();
+
+                        MessageBox.Show(index.ToString());
+
+                        return;*/
+
+                        /*var rectangle = (ListBoxItem) element;
+
+                        this._dragged = new Rectangle()
+                        {
+                            Width = rectangle.Width,
+                            Height = rectangle.Height
+                        };
+                        
+                        DragDrop.DoDragDrop(this._dragged, this._dragged, DragDropEffects.Move);
+
+                        break;*/
+                    }
+
+                    element = VisualTreeHelper.GetParent(element) as UIElement;
+
+                    if (element is ListBox)
+                        return;
+                }
+            };
+            
+            this.CurrentProgramm.Drop += (sender, e) =>
+            {
+                //this.CurrentCommands.Add(this._dragged);
+                this._dragged = null;
+
+                if (!this.CurrentCommands.Any())
+                    return;
+                
+                var element = AllCommands.InputHitTest(e.GetPosition(this.CurrentProgramm)) as UIElement;
+                
+                while (element != null)
+                {
+                    if (element is Rectangle)
+                    {
+                        var rectangle = (Rectangle)element;
+                        
+                        break;
+                    }
+
+                    element = VisualTreeHelper.GetParent(element) as UIElement;
+                }
+            };
 
             var robot = new Robot(new Transform());
             robot.Processor.Commands.AddRange(new Action[]
@@ -51,29 +143,29 @@ namespace TeachMe.Appl
                 robot.Forward,
                 robot.Leftward
             });
-
-            var gameModel = new GameModel(robot,
+            
+            this._gameModel = new GameModel(robot,
                 new Field(5, 5));
+            
+            this.RobotCommands = new ObservableCollection<Command>
+            {
+                new Command(0,
+                    "Вперед",
+                    "Двигаться на 1 клетку вперед",
+                    new BitmapImage(new Uri("CommandImages/Forward.png", UriKind.Relative)),
+                    robot.Forward),
+                new Command(1,
+                    "Назад",
+                    "Двигаться на 1 клетку назад",
+                    new BitmapImage(new Uri("CommandImages/Backward.png", UriKind.Relative)),
+                    robot.Forward)
+            };
+            this.CurrentCommands = new ObservableCollection<Command>();
 
-            var data = new List<Button>();
-            data.Add(new Button {Content = "Нажми на меня"});
-            data.Add(new Button {Content = "Нажми на меня 2"});
-            data.Add(new Button {Content = "Нажми на меня 2"});
-            data.Add(new Button {Content = "Нажми на меня 2"});
-            data.Add(new Button {Content = "Нажми на меня 2"});
-            data.Add(new Button {Content = "Нажми на меня 2"});
-            data.Add(new Button {Content = "Нажми на меня 2"});
-            data.Add(new Button {Content = "Нажми на меня 2"});
-            data.Add(new Button {Content = "Нажми на меня 2"});
-            data.Add(new Button {Content = "Нажми на меня 2"});
-            data.Add(new Button {Content = "Нажми на меня 2"});
-            data.Add(new Button {Content = "Нажми на меня 2"});
-            data.Add(new Button {Content = "Нажми на меня 2"});
-            data.Add(new Button {Content = "Нажми на меня 2"});
-            data.Add(new Button {Content = "Нажми на меня 2"});
-            data.Add(new Button {Content = "Нажми на меня 2"});
+            this.AllCommands.ItemsSource = this.RobotCommands;
+            this.CurrentProgramm.ItemsSource = this.CurrentCommands;
 
-            this.CurrentProgramm.ItemsSource = data;
+            // Нужно бы сделать класс который подкачивает все команды робота и записывает их в нужный лист бокс)
         }
     }
 }
